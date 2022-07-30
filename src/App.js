930a0e51e66f987a8db60b585c53eda9122b1670
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-07-30 16:45:58 -0400

    added a working global state using context api in the app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+// imports for app js
 import "./App.css";
+// import state and context api
+import { createContext, useState } from "react";
+// importing the search bar component
+import NewsSearchBar from "./components/searchNews";
 
+// creating the context for the news api
+export const newsAPI = createContext();
 function App() {
+  // creating state to hold the news api data
+  const [newsAPIData, setNewsAPIData] = useState();
+  console.log(newsAPIData);
+
   return (
-    <div className="app">
-      <header className="app-header">
-        <h1 className="app-head-title"> News Finder</h1>
-      </header>
-    </div>
+    // wrapping all of the app's code with the context api, using state to store the data of the news api search results
+    <newsAPI.Provider value={{ newsAPIData, setNewsAPIData }}>
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-head-title"> News Finder</h1>
+        </header>
+        <p className="app-text">
+          Use The search bar below to find articles in the topics of your
+          choice!
+        </p>
+        <NewsSearchBar />
+      </div>
+    </newsAPI.Provider>
   );
 }
 
