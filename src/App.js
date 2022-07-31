5fa07bb78d758114d739db2033b4ca1cad3ec562
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-07-30 20:42:49 -0400

    added some new state to the context api want to include a loading animation in the future with react-loader npm

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import "./App.css";
 import { createContext, useState } from "react";
 // importing the search bar component
 import NewsSearchBar from "./components/searchNews";
+// import the news cards component
+import NewsCards from "./components/newsCards";
 
 // creating the context for the news api
 export const newsAPI = createContext();
 function App() {
   // creating state to hold the news api data
   const [newsAPIData, setNewsAPIData] = useState();
-  console.log(newsAPIData);
+  const [loading, setLoading] = useState();
 
   return (
     // wrapping all of the app's code with the context api, using state to store the data of the news api search results
-    <newsAPI.Provider value={{ newsAPIData, setNewsAPIData }}>
+    <newsAPI.Provider
+      value={{ newsAPIData, setNewsAPIData, loading, setLoading }}
+    >
       <div className="app">
         <header className="app-header">
           <h1 className="app-head-title"> News Finder</h1>
@@ -24,6 +28,7 @@ function App() {
           choice!
         </p>
         <NewsSearchBar />
+        <NewsCards />
       </div>
     </newsAPI.Provider>
   );
